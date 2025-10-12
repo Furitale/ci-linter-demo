748f2d7ddc0a8cb24699f0911b9a3a900517a41c
Furitale <2208544961@qq.com>
2025-10-12 19:57:12 +0800

    feat: add subtraction function

--- calculator.py
+++ calculator.py
@@ -1,8 +1,12 @@
-# def add(a, b):
-#     """返回两个数字的和"""
-#     return a + b
-
-
-# 修改calculator.py中的add函数
 def add(a, b):
-    return a + b + 1  # 引入错误
+    """返回两个数字的和"""
+    return a + b
+
+
+def subtract(a, b):
+    """返回两个数字的差"""
+    return a - b
+
+# # 修改calculator.py中的add函数
+# def add(a, b):
+#     return a + b + 1  # 引入错误
